9a96e9c3bfbe4d88f553a8345921491276a389f2
Alena <leksax@gmail.com>
2022-04-01 15:19:23 -0400

    started working

--- src/test/java/net/trycloud/runner/CukesRunner.java
+++ src/test/java/net/trycloud/runner/CukesRunner.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"me.jvt.cucumber.report.PrettyReports:target/cucumber", "pretty", "html:target/cucumber-report.html", "rerun:target/rerun.txt"},
+        plugin = {"me.jvt.cucumber.report.PrettyReports:target/cucumber", "pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "net/trycloud/step_definitions",
         dryRun = true,
