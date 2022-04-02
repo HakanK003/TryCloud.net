b99b1b4d6e0b5319b4ccb08b672d4dac36bc62e8
Alena <leksax@gmail.com>
2022-04-02 18:00:10 -0400

    Working on US12 continue

--- src/test/java/net/trycloud/runner/CukesRunner.java
+++ src/test/java/net/trycloud/runner/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "net/trycloud/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@wipAP",
         publish = true
 )
 
---
+++ src/test/java/net/trycloud/step_definitions/ContactsModuleAccessStepDefinitions.java
@@ -0,0 +1,48 @@
+package net.trycloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.trycloud.pages.HomePage;
+import net.trycloud.pages.LoginPage;
+import net.trycloud.utilities.BrowserUtils;
+import net.trycloud.utilities.ConfigurationReader;
+import net.trycloud.utilities.Driver;
+import org.junit.Assert;
+
+public class ContactsModuleAccessStepDefinitions {
+
+     LoginPage loginPage = new LoginPage();
+     HomePage homePage = new HomePage();
+
+
+
+     @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("environment"));
+
+        loginPage.usernameInput.sendKeys(ConfigurationReader.getProperty("user"));
+        loginPage.passwordInput.sendKeys( ConfigurationReader.getProperty("password") );
+        loginPage.LogInBTN.click();
+    }
+
+
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String string) {
+
+        homePage.contactsModule.click();
+
+    }
+    @Then("verify the page title is {string}")
+    public void verify_the_page_title_is(String string) {
+
+        BrowserUtils.sleep(3);
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains("contacts trycloud qa"));
+
+
+
+    }
+
+}
